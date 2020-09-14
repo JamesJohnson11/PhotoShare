d930cae29e28cde5b11826df82b354c50715a4a6
Barry Johnson <bdotmgmt@gmail.com>
2020-09-14 17:59:27 -0400

    Created custom authentication file

--- account/views.py
+++ account/views.py
@@ -54,9 +54,11 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
-            messages.success(request, 'Profile updated '\'successfully')
+            messages.success(request, 'Profile updated successfully')
+            print('Success!')
         else:
             messages.error(request, 'Error updating your profile')
+            print('Error!')
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
